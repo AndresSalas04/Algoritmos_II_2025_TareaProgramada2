bd26b242874df99f2b1760056509cdc192653d9a
Agustin <aguterralgo@gmail.com>
2025-12-07 12:22:18 -0600

    Documentacion de la clase tiempo

--- tiempo.py
+++ tiempo.py
@@ -8,14 +8,26 @@ import time
 from typing import Dict
 
 class MedidorTiempo:
-    
+    """
+    Medidor de tiempo usando perf_counter_ns().
+    """
     def __init__(self):
+        """Inicializa el atributo que almacena el tiempo de inicio."""
         self.inicio_ns = 0
 
     def cargar_tiempo(self):
+        """
+        Guarda el tiempo actual como tiempo de inicio.
+        """
         self.inicio_ns = time.perf_counter_ns()
 
     def intervalo_tiempo(self) -> Dict[str, float]:
+        """
+        Calcula el intervalo de tiempo desde cargar_tiempo() hasta ahora.
+
+        Returns: un valor flotante que representa el tiempo en milisegundos
+        """
+
         fin = time.perf_counter_ns()
         ns = fin - self.inicio_ns
 
@@ -27,4 +39,9 @@ class MedidorTiempo:
         }
 
     def formato_tiempo(self, tiempo: Dict[str, float]) -> str:
+        """
+        Convierte el tiempo calculado en una cadena legible.
+
+        Returns: str: Tiempo formateado con 3 decimales, en milisegundos.
+        """
         return f"{tiempo['ms']:.3f} ms"
